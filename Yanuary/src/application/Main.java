3454a7217cabc1c68837de856e1c37e15ccc1fbd
manssonskij <anton.mansson@gmail.com>
2016-01-09 18:59:13 +0100

    Added circle

--- Yanuary/src/application/Main.java
+++ Yanuary/src/application/Main.java
@@ -4,17 +4,29 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Pane root = new Pane();
+			Scene scene = new Scene(root,primaryStage.getWidth(),primaryStage.getHeight());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setFullScreen(true);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			Circle circle=new Circle();
+			circle.setCenterX(root.getWidth()/2);
+			circle.setCenterY(root.getHeight()/2);
+			circle.setRadius(50);
+			circle.setFill(Color.BISQUE);
+			root.getChildren().add(circle);
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
